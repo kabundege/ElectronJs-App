dca8f40bebf4af1cac9810108e01eb989cf9d5a1
kabundege <christophekwizera1@gmail.com>
2020-11-24 11:30:13 +0200

    app icon integration

--- main.js
+++ main.js
@@ -1,28 +1,65 @@
-const { app, BrowserWindow } = require('electron')
+const { app,BrowserWindow,Tray ,nativeImage} = require('electron')
+const path = require('path')
 
-function createWindow () {
-  const win = new BrowserWindow({
-    width: 800,
-    height: 600,
-    webPreferences: {
-      nodeIntegration: true
-    }
-  })
+let tray,window ;
 
-  win.loadFile('index.html')
-  win.webContents.openDevTools()
+// app.dock.hide()
+
+const createWindow = () => {
+    window = new BrowserWindow({
+        width:800,
+        height:600,
+        show:true,
+        frame:false,
+        resizable:false,
+        transparent:false,
+        fullscreenable:false,
+        webPreferences:{
+            nodeIntegration: true,
+        },
+    });
+
+    window.on('close',() => win = null)
 }
+
+const createTray = () => {
+    const icon = path.join(_dirname,'./icon.png');
+    const nImage = nativeImage.createFromPath(icon);
+
+    tray = new Tray(nImage);
+    tray.on('click',(e)=> toggleWindow())
+}
+
+const toggleWindow = () => window.isVisible() ? window.hide() : showWindow();
+
+const showWindow = () => {
+    const { x,y } = windowPosition();
+    window.setPosition(x,y)
+    window.show()
+}
+
+const windowPosition = () => {
+    const windowBounds = window.getBounds();
+    const trayBounds = tray.getBounds();
+
+    const x = Math.round(trayBounds.x + (trayBounds.width/2) - (windowBounds.width/2))
+    const y = Math.round(trayBounds.y + trayBounds.height)
+
+    return { x,y }
+}
+
 
 app.whenReady().then(createWindow)
 
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
+app.on('window-all-closed',()=>{
+    if(process.platform !== 'darwin'){
+        app.quit()
+    }
 })
 
-app.on('activate', () => {
-  if (BrowserWindow.getAllWindows().length === 0) {
-    createWindow()
-  }
+app.on('activate',()=>{
+    if(BrowserWindow.getAllWindows.length === 0){
+        createTray()
+        createWindow()
+    }
 })
